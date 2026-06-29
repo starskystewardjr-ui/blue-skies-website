a60df0ff80602190cac1c5fc89627aa3897ae3c4
starskystewardjr-ui <starskystewardjr@gmail.com>
2026-06-28 22:08:16 -0400

    q

---
+++ components/Footer.tsx
@@ -0,0 +1,9 @@
+export default function Footer() {
+  return (
+    <footer className="footer">
+      <h3>Blue Skies Mobile Wash & Home Care</h3>
+      <p>Professional Mobile Detailing</p>
+      <p>Columbia County & Surrounding Areas</p>
+    </footer>
+  );
+}

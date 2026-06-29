b1b071ec032a1b0019335add9f6609cf2bef9577
starskystewardjr-ui <starskystewardjr@gmail.com>
2026-06-28 23:57:56 -0400

    q

--- app/page.tsx
+++ app/page.tsx
@@ -88,3 +88,5 @@ export default function Home() {
 
   </div>
 </section>
+<CTA />
+<Footer />
--- components/Footer.tsx
+++ components/Footer.tsx
@@ -2,8 +2,21 @@ export default function Footer() {
   return (
     <footer className="footer">
       <h3>Blue Skies Mobile Wash & Home Care</h3>
+
       <p>Professional Mobile Detailing</p>
       <p>Columbia County & Surrounding Areas</p>
+
+      <p>
+        <a href="tel:5709125307">570-912-5307</a>
+      </p>
+
+      <p>
+        <a href="mailto:info.blueskiesmobilewash@gmail.com">
+          info.blueskiesmobilewash@gmail.com
+        </a>
+      </p>
+
+      <p>Founded by Starsky Steward Jr.</p>
     </footer>
   );
 }
